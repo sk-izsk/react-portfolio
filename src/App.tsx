52c959e0888b3f6912059977160b8e56499cf6c0
izsk <sk.zeeshan1992@gmail.com>
2023-08-19 21:23:51 -0400

    add add themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Header } from "./components"
+import "./themes/themes.css"
 
 interface Props {}
 
 const App: React.FC<Props> = () => {
+  useEffect(() => {
+    if (localStorage.getItem("theme")) {
+      document.documentElement.setAttribute(
+        "data-theme",
+        localStorage.getItem("theme") as string
+      )
+    }
+    // document.documentElement.setAttribute("data-theme", "github-dark")
+    // localStorage.setItem("theme", "github-dark")
+  }, [])
   return (
     <>
       <Header />
